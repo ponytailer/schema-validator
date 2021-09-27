4592ad9bf6d4869f898d0dea865c28cfc265415e
huangsong <huangsong@leyantech.com>
2021-09-27 19:12:58 +0800

    remove mixin

--- schema_validator/__init__.py
+++ schema_validator/__init__.py
@@ -1,5 +1,4 @@
 from .extension import FlaskSchema
-from .mixins import SchemaValidationError
 from .typing import ResponseReturnValue
 from .validation import DataSource, validate
 from .command import generate_schema_command
@@ -8,7 +7,6 @@ __all__ = (
     "DataSource",
     "FlaskSchema",
     "ResponseReturnValue",
-    "SchemaValidationError",
     "validate",
     "generate_schema_command"
 )
