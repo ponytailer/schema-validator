8e529cb484b068c5ea7bcaa83e0daa8aac2b01f4
huangsong <huangsong@leyantech.com>
2021-09-27 17:48:22 +0800

    add doc

--- schema_validator/validation.py
+++ schema_validator/validation.py
@@ -127,6 +127,46 @@ def validate(
             the body source
         response:
             response model define
+
+    from dataclasses import dataclass
+    from datetime import datetime
+    from typing import Optional
+    from pydantic import BaseModel
+
+    from flask import Flask
+    from schema_validator import FlaskSchema, validate
+
+    app = Flask(__name__)
+
+    FlaskSchema(app)
+    OR
+
+    schema = FlaskSchema()
+    schema.init_app(app)
+
+    @dataclass
+    class Todo:
+        task: str
+        due: Optional[datetime]
+
+    class TodoResponse(BaseModel):
+        id: int
+        name: str
+
+    @app.post("/")
+    @validate(body=Todo, responses=TodoResponse)
+    def create_todo():
+        ... # Do something with data, e.g. save to the DB
+        return dict(id=1, name="2")
+
+    @app.put("/")
+    @validate(
+        body=Todo,
+        responses={200: TodoResponse, 400: TodoResponse}
+    )
+    def update_todo():
+        ... # Do something with data, e.g. save to the DB
+        return TodoResponse(id=1, name="123")
     """
     if validate_path:
         pass
