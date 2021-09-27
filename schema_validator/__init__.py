966945e2f7eb837571059172ea1bab13b4106612
huangsong <huangsong@leyantech.com>
2021-09-27 13:22:20 +0800

    fix

--- schema_validator/__init__.py
+++ schema_validator/__init__.py
@@ -1,23 +1,12 @@
 from .extension import FlaskSchema
 from .mixins import SchemaValidationError
 from .typing import ResponseReturnValue
-from .validation import (
-    DataSource,
-    RequestSchemaValidationError,
-    ResponseSchemaValidationError,
-    validate_querystring,
-    validate_request,
-    validate_response,
-)
+from .validation import DataSource, validate
 
 __all__ = (
     "DataSource",
     "FlaskSchema",
-    "RequestSchemaValidationError",
     "ResponseReturnValue",
-    "ResponseSchemaValidationError",
     "SchemaValidationError",
-    "validate_querystring",
-    "validate_request",
-    "validate_response",
+    "validate",
 )
--- schema_validator/validation.py
+++ schema_validator/validation.py
@@ -1,40 +1,23 @@
 from dataclasses import asdict, is_dataclass
 from enum import Enum, auto
 from functools import wraps
-from typing import Any, Callable, Optional, Union, cast
+from typing import Any, Callable, Dict, Optional, Union, cast
 
 from pydantic import BaseModel, ValidationError
 from pydantic.dataclasses import dataclass as pydantic_dataclass, \
     is_builtin_dataclass
 from pydantic.schema import model_schema
-from flask import Response, request
+from flask import Response, jsonify, request, g
 from werkzeug.datastructures import Headers
 from werkzeug.exceptions import BadRequest
 
 from .constants import SCHEMA_QUERYSTRING_ATTRIBUTE, \
     SCHEMA_REQUEST_ATTRIBUTE, SCHEMA_RESPONSE_ATTRIBUTE
-from .typing import PydanticModel, ResponseReturnValue
+from .typing import PydanticModel
 
 
 class SchemaInvalidError(Exception):
     pass
-
-
-class ResponseSchemaValidationError(Exception):
-    def __init__(
-        self,
-        validation_error: Optional[ValidationError] = None
-    ) -> None:
-        self.validation_error = validation_error
-
-
-class RequestSchemaValidationError(BadRequest):
-    def __init__(
-        self,
-        validation_error: Union[TypeError, ValidationError]
-    ) -> None:
-        super().__init__()
-        self.validation_error = validation_error
 
 
 class DataSource(Enum):
@@ -42,116 +25,146 @@ class DataSource(Enum):
     JSON = auto()
 
 
-def validate_querystring(model_class: PydanticModel) -> Callable:
-    if is_builtin_dataclass(model_class):
-        model_class = pydantic_dataclass(model_class)
-
-    schema = model_schema(model_class)
-
-    if len(schema.get("required", [])) != 0:
+def check_query_string_schema(query_string: PydanticModel) -> PydanticModel:
+    if is_builtin_dataclass(query_string):
+        query_string = pydantic_dataclass(query_string).__pydantic_model__
+    query_string_schema = model_schema(query_string)
+    if len(query_string_schema.get("required", [])) != 0:
         raise SchemaInvalidError("Fields must be optional")
-
-    def decorator(func: Callable) -> Callable:
-        setattr(func, SCHEMA_QUERYSTRING_ATTRIBUTE, model_class)
-
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            try:
-                model = model_class(**request.args)
-            except (TypeError, ValidationError) as error:
-                raise RequestSchemaValidationError(error)
-            else:
-                return func(*args, query_args=model, **kwargs)
-
-        return wrapper
-
-    return decorator
+    return query_string
 
 
-def validate_request(
-    model_class: PydanticModel,
-    *,
-    source: DataSource = DataSource.JSON,
-) -> Callable:
-    if is_builtin_dataclass(model_class):
-        model_class = pydantic_dataclass(model_class)
+def check_body_schema(
+    body: PydanticModel,
+    source: DataSource
+) -> PydanticModel:
+    if is_builtin_dataclass(body):
+        body = pydantic_dataclass(body).__pydantic_model__
 
-    schema = model_schema(model_class)
+    schema = model_schema(body)
     if source == DataSource.FORM and any(
         schema["properties"][field]["type"] == "object" for field in
             schema["properties"]
     ):
         raise SchemaInvalidError("Form must not have nested objects")
+    return body
 
-    def decorator(func: Callable) -> Callable:
-        setattr(func, SCHEMA_REQUEST_ATTRIBUTE, (model_class, source))
 
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            data = request.get_json() if source == DataSource.JSON else request.form  # noqa
+def check_response(result, response_model: Dict[int, PydanticModel]):
+    status_or_headers: Union[None, int, str, Dict, list] = None
+    headers: Optional[Headers] = None
+
+    if isinstance(result, tuple):
+        value, status_or_headers, headers = result + (None,) * (
+            3 - len(result))
+    else:
+        value = result
+
+    status = 200
+    if status_or_headers is not None and not isinstance(
+        status_or_headers, (Headers, dict, list)
+    ) and str(status_or_headers).isdigit():
+        status = int(status_or_headers)
+
+    for status_code, model_cls in response_model.items():
+        if status_code != status:
+            continue
+        if isinstance(value, dict):
             try:
-                model = model_class(**data)
-            except (TypeError, ValidationError) as error:
-                raise RequestSchemaValidationError(error)
-            else:
-                return func(*args, data=model, **kwargs)
+                model_value = model_cls(**value)
+            except (TypeError, ValidationError) as ve:
+                return jsonify(validation_error=str(ve.errors())), BadRequest
+        elif type(value) == model_cls:
+            model_value = value
+        elif is_builtin_dataclass(value):
+            model_value = model_cls(**asdict(value))
+        else:
+            return jsonify(validation_error="invalid response"), BadRequest
+        if is_dataclass(model_value):
+            return asdict(model_value), status_or_headers, headers
+        else:
+            model_value = cast(BaseModel, model_value)
+            return model_value.dict(), status_or_headers, headers
+    return result
 
-        return wrapper
 
-    return decorator
-
-
-def validate_response(
-    model_class: PydanticModel,
-    status_code: int = 200
+def validate(
+    query_string: Optional[PydanticModel] = None,
+    body: Optional[PydanticModel] = None,
+    source: DataSource = DataSource.JSON,
+    validate_path: bool = False,
+    response: Optional[PydanticModel] = None,
+    responses: Dict[int, Optional[PydanticModel]] = None,
+    headers: Optional[PydanticModel] = None
 ) -> Callable:
-    if is_builtin_dataclass(model_class):
-        model_class = pydantic_dataclass(model_class)
+    """
+    params:
+        query_string:
+            the params in query
+        body:
+            json body or form
+        source:
+            the body source
+        response:
+            response model define
+    TODO:
+        validate_path:
+            check the params in path
+        headers:
+            headers model define
+    """
+    if validate_path:
+        pass
+    if headers is not None:
+        pass
 
-    def decorator(
-        func: Callable[..., ResponseReturnValue]
-    ) -> Callable[..., Response]:
-        schemas = getattr(func, SCHEMA_RESPONSE_ATTRIBUTE, {})
-        schemas[status_code] = model_class
-        setattr(func, SCHEMA_RESPONSE_ATTRIBUTE, schemas)
+    if query_string is not None:
+        query_string = check_query_string_schema(query_string)
+
+    if body is not None:
+        body = check_body_schema(body, source)
+
+    def decorator(func: Callable) -> Callable[..., Response]:
+
+        if query_string:
+            setattr(func, SCHEMA_QUERYSTRING_ATTRIBUTE, query_string)
+        if body:
+            setattr(func, SCHEMA_REQUEST_ATTRIBUTE, (body, source))
 
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
+            err = {}
+            if body:
+                data = request.get_json() if source == DataSource.JSON else request.form  # noqa
+                try:
+                    body_model = body(**data)
+                except (TypeError, ValidationError) as ve:
+                    err["body_params"] = str(ve)
+                else:
+                    g.body_params = body_model
+
+            if query_string:
+                print(request.args, query_string)
+                try:
+                    query_params = query_string(**request.args)
+                except (TypeError, ValidationError) as ve:
+                    err["query_params"] = str(ve)
+                else:
+                    g.query_params = query_params
+
+            if err:
+                return jsonify(validation_error=err), BadRequest.code
+
             result = func(*args, **kwargs)
 
-            status_or_headers = None
-            headers = None
-            if isinstance(result, tuple):
-                value, status_or_headers, headers = result + (None,) * (
-                    3 - len(result))
-            else:
-                value = result
-
-            status = 200
-            if status_or_headers is not None and not isinstance(
-                status_or_headers, (Headers, dict, list)
-            ):
-                status = int(status_or_headers)
-
-            if status == status_code:
-                if isinstance(value, dict):
-                    try:
-                        model_value = model_class(**value)
-                    except ValidationError as error:
-                        raise ResponseSchemaValidationError(error)
-                elif type(value) == model_class:
-                    model_value = value
-                elif is_builtin_dataclass(value):
-                    model_value = model_class(**asdict(value))
-                else:
-                    raise ResponseSchemaValidationError()
-                if is_dataclass(model_value):
-                    return asdict(model_value), status_or_headers, headers
-                else:
-                    model_value = cast(BaseModel, model_value)
-                    return model_value.dict(), status_or_headers, headers
-            else:
-                return result
+            if responses:
+                setattr(func, SCHEMA_RESPONSE_ATTRIBUTE, responses)
+                return check_response(result, responses)
+            if response:
+                resp = {200: response}
+                setattr(func, SCHEMA_RESPONSE_ATTRIBUTE, resp)
+                return check_response(result, resp)
+            return result
 
         return wrapper
 
